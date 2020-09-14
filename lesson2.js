64f23527e168c4cbdf80e9362e83928ab5e292c7
albert8686 <71070817+albert8686@users.noreply.github.com>
2020-09-14 14:32:55 +0400

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,19 @@
+const PI=Math.PI;
+
+let r=parseFloat(prompt("Enter perimeter of a circle"));
+
+// perimeter of a circle
+
+let RadiusCircle = r/ (2 * PI);
+
+alert("decision"+' '+ RadiusCircle);
+
+
+//area of the circle 
+
+let  r2 = parseFloat(prompt('Enter area of the circle'))
+
+let Area_Circle = r * r * PI;
+
+alert("decision"+' '+Area_Circle);
+ 
