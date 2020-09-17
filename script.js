b067b3a3c57434fe19765721eb767e9b4adac0ea
albert8686 <71070817+albert8686@users.noreply.github.com>
2020-09-17 22:49:33 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-alert('hello Wolrd');
+//Task 1
+
+// let user_password= prompt('Enter your password');
+// if(user_password.length >= 5){
+//     alert('Welcome'+' '+user_password);
+// }
+// else{
+
+//     alert('Your password must be at least 5 letters')
+
+// }
+
+// task 2
+
+let text = prompt("Enter text")
+
+let result=0;
+switch(text[2]){
+    case 'a':
+        result+=4
+        break;
+    case 'e':
+        result+=4
+        break;
+        case 'u':
+            result+=4
+            break;
+        case 'i':
+            result+=4
+            break;
+        case 'o':
+            result+=4
+            break;
+        case 'z':
+            result+=5
+            break;
+            
+    default :
+    result-=1   
+ 
+}
+
+
+alert('my result number'+' '+result);
